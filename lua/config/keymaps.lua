5c02b84b2072a0f628db85b39983515ad3408f0d
ThiagooAndrade <andradethiago1609@gmail.com>
2026-02-05 15:52:29 -0300

    telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -139,3 +139,13 @@ end, { nargs = 1 })
 
 -- Aerial nvim
 vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+
+vim.keymap.set("n", "<space>fa", ":Telescope file_browser<CR>")
+
+-- open file_browser with the path of the current buffer
+vim.keymap.set("n", "<space>fa", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+
+-- Alternatively, using lua API
+vim.keymap.set("n", "<space>fa", function()
+	require("telescope").extensions.file_browser.file_browser()
+end)
